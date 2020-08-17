0980587b39a60f4942d1f8231340fec16819ace1
Emanuel Dorigon <emanueldorigon123@gmail.com>
2020-08-17 14:27:29 -0300

    Finish project

--- streamWriter/Program.cs
+++ streamWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace streamWriter
 {
@@ -6,7 +7,27 @@ namespace streamWriter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourcepath = @"C:\Users\Emanuel\OneDrive - Universidade Federal do Rio Grande do Sul\Cources and others\C#\Text\File1.txt";
+            string targetpath = @"C:\Users\Emanuel\OneDrive - Universidade Federal do Rio Grande do Sul\Cources and others\C#\Text\File2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
